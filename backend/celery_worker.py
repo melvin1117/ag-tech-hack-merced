ab355f16c501ede74b5ea5318d1d284f7e3edbc1
Aatisha Cyrill <cyrill@wisc.edu>
2025-03-08 00:59:19 -0800

    docker updated

--- backend/celery_worker.py
+++ backend/celery_worker.py
@@ -4,7 +4,7 @@ from celery import Celery
 celery_app = Celery(
     "worker",
     broker="redis://redis:6379/0",
-    backend="mongodb://mongo:27017/celery_results"
+    backend="mongodb://admin:secret@mongo:27017/celery_results?authSource=admin"
 )
 
 @celery_app.task
